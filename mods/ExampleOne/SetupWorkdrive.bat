dce1352f845c17032b5408cd856530734040f9c3
hub33k <hub33k@yahoo.com>
2020-12-04 12:36:44 +0100

    Setup filepatching folders for ExampleOne (create symlinks).

--- mods/ExampleOne/SetupWorkdrive.bat
+++ mods/ExampleOne/SetupWorkdrive.bat
@@ -6,6 +6,9 @@ set "workDrive=P:"
 set "prefix=HM"
 set "modName=ExampleOne"
 
+set "clientDir=%workDrive%\Client"
+set "serverDir=%workDrive%\Server"
+
 if not exist "%workDrive%\%prefix%\" (
   echo Creating folder %workDrive%\%prefix%\
   mkdir "%workDrive%\%prefix%"
@@ -18,6 +21,20 @@ if exist "%workDrive%\%prefix%\%modName%\" (
 
 echo Creating link %workDrive%\%prefix%\%modName%
 mklink /J "%workDrive%\%prefix%\%modName%\" "%cd%\%prefix%\%modName%\"
+
+@REM Setup filepatching folders (Client and Server)
+
+if exist "%clientDir%\%prefix%" (
+  echo Removing existing link "%clientDir%\%prefix%"
+  rmdir "%clientDir%\%prefix%"
+)
+mklink /J "%clientDir%\%prefix%\" "%workDrive%\%prefix%\"
+
+if exist "%serverDir%\%prefix%" (
+  echo Removing existing link "%serverDir%\%prefix%"
+  rmdir "%serverDir%\%prefix%"
+)
+mklink /J "%serverDir%\%prefix%\" "%workDrive%\%prefix%\"
 
 echo Done
 
