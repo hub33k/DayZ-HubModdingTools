841f2078e800a58692d60dc6fbd9b675425029d3
hub33k <hub33k@yahoo.com>
2021-11-23 20:16:12 +0100

    Implement GenerateGproj

--- scripts/GenerateGproj.ps1
+++ scripts/GenerateGproj.ps1
@@ -2,12 +2,13 @@ Set-StrictMode -Version Latest
 
 . $PSScriptRoot/Config
 
-Clear-Host
-
 # NOTE (hub33k): copy of from C:\Program Files (x86)\Steam\steamapps\common\DayZ\dayz.gproj
 #  fixed indentation
 $GprojTemplate = "$PSScriptRoot/files/dayz-template.gproj"
 
+$ModPrefix = "$($MODS_TO_BUILD[0].Prefix)"
+$ModName = "$($MODS_TO_BUILD[0].ModName)".Replace("@", "")
+$ModPrefixLinkRoot = "$($MODS_TO_BUILD[0].PrefixLinkRoot)"
 $CurrentModPath = "$ModRootPath\$($MODS_TO_BUILD[0].PrefixLinkRoot)"
 $GrojOutputFile = "$CurrentModPath\Workbench\dayz.gproj"
 
@@ -18,8 +19,72 @@ $GrojOutputFile = "$CurrentModPath\Workbench\dayz.gproj"
 #   $GrojOutputFile = "$ModRootPath\$($mod.PrefixLinkRoot)\Workbench\dayz.gproj"
 # }
 
-$CurrentModPath
-$GrojOutputFile
-# Get-Content -Path $GprojTemplate
+$foldersToAdd = @(
+  "1_Core"
+  "2_GameLib"
+  "3_Game"
+  "4_World"
+  "5_Mission"
+)
 
-# pause
+$variablesToReplace = @(
+  "@GAME_DIR"
+  "@IMAGE_SET"
+  "@WIDGET_STYLE"
+  "@SCRIPTS_1_CORE"
+  "@SCRIPTS_2_GAMELIB"
+  "@SCRIPTS_3_GAME"
+  "@SCRIPTS_4_WORLD"
+  "@SCRIPTS_5_MISSION"
+  "@SCRIPTS_WORKBENCH"
+)
+
+# Object of arrays of strings
+[pscustomobject] $scriptsPaths = @{}
+$scriptsPaths["GAME_DIR"] = [System.Collections.Arraylist] @()
+$scriptsPaths["IMAGE_SET"] = [System.Collections.Arraylist] @()
+$scriptsPaths["WIDGET_STYLE"] = [System.Collections.Arraylist] @()
+$scriptsPaths["SCRIPTS_1_CORE"] = [System.Collections.Arraylist] @()
+$scriptsPaths["SCRIPTS_2_GAMELIB"] = [System.Collections.Arraylist] @()
+$scriptsPaths["SCRIPTS_3_GAME"] = [System.Collections.Arraylist] @()
+$scriptsPaths["SCRIPTS_4_WORLD"] = [System.Collections.Arraylist] @()
+$scriptsPaths["SCRIPTS_5_MISSION"] = [System.Collections.Arraylist] @()
+$scriptsPaths["SCRIPTS_WORKBENCH"] = [System.Collections.Arraylist] @()
+
+
+# ================================================================
+# Handle paths
+
+Get-ChildItem -Path "$CurrentModPath\*" -Recurse -Depth 1 | ForEach-Object {
+  foreach ($folder in $foldersToAdd) {
+    if ($_.Name -eq $folder) {
+      $test = "$ModPrefixLinkRoot\" + $_.FullName.Replace("$CurrentModPath\", "")
+      $test = $test.Replace("\", "/")
+      $scriptsPaths[$("SCRIPTS_$($_.Name)")].Add("`"$test`"") | Out-Null
+    }
+  }
+}
+
+
+# ================================================================
+# Add entries to gproj file
+
+$GprojTemplateFile = Get-Content -Path $GprojTemplate
+
+"Generating dayz.gproj file: $GrojOutputFile"
+
+$GprojTemplateFile | ForEach-Object {
+  if ($variablesToReplace.Contains($_)) {
+    $array = $scriptsPaths[$($_.Replace("@", ""))]
+    if ($array) {
+      # indent first element
+      $array[0] = "`n`t`t`t`t`t`t$($array[0])"
+      $array -join  "`n`t`t`t`t`t`t"
+    }
+  }
+  else {
+    $_
+  }
+} | Set-Content -Path "$GrojOutputFile"
+
+pause
